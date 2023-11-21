41ea79996aad422a2292e72db17010811ceb133f
liuchengyue <17554334529@163.com>
2023-11-21 17:02:34 +0800

    数据准备

--- data_preparation/file_path_manager.py
+++ data_preparation/file_path_manager.py
@@ -5,6 +5,7 @@ import os
 from loguru import logger
 
 from settings import DEBIAN_FILE_DIR_PATH
+from tqdm import tqdm
 
 
 # @Time : 2023/11/21 16:48
@@ -24,17 +25,14 @@ def parse_info_from_file_name(file_name):
 def get_tar_file_paths():
     src_tar_paths = []
     bin_tar_paths = []
-    count = 0
-    for root, dirs, files in os.walk(DEBIAN_FILE_DIR_PATH):
-        for f_name in files:
-            count += 1
-            if count % 1000 == 0:
-                logger.info(f"walk progress: {count}")
+    with tqdm(total=1390665, desc="get_tar_file_paths") as pbar:
+        for root, dirs, files in os.walk(DEBIAN_FILE_DIR_PATH):
+            for f_name in files:
+                pbar.update(1)
 
-            f_path = os.path.join(root, f_name)
-            if is_src_package(f_name):
-                src_tar_paths.append(f_path)
-            elif is_bin_package(f_name):
-                bin_tar_paths.append(f_path)
-    logger.info(f"walk finished. all count = {count}")
+                f_path = os.path.join(root, f_name)
+                if is_src_package(f_name):
+                    src_tar_paths.append(f_path)
+                elif is_bin_package(f_name):
+                    bin_tar_paths.append(f_path)
     return src_tar_paths, bin_tar_paths
