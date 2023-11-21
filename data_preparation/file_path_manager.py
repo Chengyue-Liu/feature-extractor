1b318407ce733295b9f0c47382ddb7ea985705de
liuchengyue <17554334529@163.com>
2023-11-21 17:07:42 +0800

    数据准备

--- data_preparation/file_path_manager.py
+++ data_preparation/file_path_manager.py
@@ -11,7 +11,12 @@ from tqdm import tqdm
 # @Time : 2023/11/21 16:48
 # @Author : Liu Chengyue
 def is_src_package(file_name):
-    pass
+    if "orig.tar" in file_name \
+            or file_name.endswith('.tar.gz') \
+            or (file_name.endswith('.tar.xz') and not file_name.endswith('.debian.tar.xz')):
+        return True
+
+    return False
 
 
 def is_bin_package(file_name):
@@ -35,4 +40,5 @@ def get_tar_file_paths():
                     src_tar_paths.append(f_path)
                 elif is_bin_package(f_name):
                     bin_tar_paths.append(f_path)
+    logger.success(f"src_tar_paths num: {len(src_tar_paths)}, bin_tar_paths num: {len(bin_tar_paths)}")
     return src_tar_paths, bin_tar_paths
