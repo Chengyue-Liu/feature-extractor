d1971a1080f79f38f8ca3cd276a737af7597a8b7
liuchengyue <17554334529@163.com>
2023-11-22 13:33:12 +0800

    update

--- utils/json_util.py
+++ utils/json_util.py
@@ -3,16 +3,21 @@
 import json
 import os.path
 
+from loguru import logger
+
 
 # @Time : 2023/11/21 17:36
 # @Author : Liu Chengyue
 
 
 def dump_to_json(data, f_path):
+    f_path = os.path.abspath(f_path)
     basedir, file_name = os.path.split(f_path)
     os.makedirs(basedir, exist_ok=True)
+    logger.critical(f"saving json ... DO NOT INTERRUPT!")
     with open(f_path, 'w') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
+    logger.success(f"save json succeed!")
 
 
 def load_from_json(f_path):
