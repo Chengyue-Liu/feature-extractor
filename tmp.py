140eb6e46faea56ab00884ec84341a19aef0e77f
liuchengyue <17554334529@163.com>
2023-11-30 10:36:08 +0800

    update tmp

--- tmp.py
+++ tmp.py
@@ -1,1 +1,11 @@
+import json
 
+from settings import SRC_REPOS_JSON, BIN_REPOS_JSON
+
+with open(SRC_REPOS_JSON) as f:
+    repos = json.load(f)
+    print(len(repos))
+
+with open(BIN_REPOS_JSON) as f:
+    repos = json.load(f)
+    print(len(repos))
