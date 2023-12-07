39f881280ea0e34a3a12eec44417472a95b1e5e0
liuchengyue <17554334529@163.com>
2023-12-07 11:36:32 +0800

    删除脚本

--- remove_tool.py
+++ remove_tool.py
@@ -11,8 +11,9 @@ def get_paths(root_dir):
     for root, dirs, files in os.walk(root_dir):
         for f in files:
             file_paths.append(os.path.join(root, f))
-            if (length := len(file_paths)) % 1000 == 0:
-                logger.info(f"get_paths: {length/1000}k")
+            if (num := len(file_paths)) % 10000 == 0:
+                num = num/1000
+                logger.info(f"get_paths: {num}k")
         for dir in dirs:
             dir_paths.append(os.path.join(root, dir))
     dir_paths = list(reversed(dir_paths))
