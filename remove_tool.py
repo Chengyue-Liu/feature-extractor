addca4e60b33488070a845a8df617b1833983eb5
liuchengyue <17554334529@163.com>
2023-12-07 11:44:25 +0800

    optimize

--- remove_tool.py
+++ remove_tool.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import os
 import shutil
 
@@ -6,33 +7,25 @@ from tqdm import tqdm
 
 
 def get_paths(root_dir):
-    file_paths = []
     dir_paths = []
     for root, dirs, files in os.walk(root_dir):
-        for f in files:
-            file_paths.append(os.path.join(root, f))
-            if (num := len(file_paths)) % 10000 == 0:
-                num = num/1000
-                logger.info(f"get_paths: {num}k")
         for dir in dirs:
             dir_paths.append(os.path.join(root, dir))
+            if (num := len(dir_paths)) % 10000 == 0:
+                num = num/1000
+                logger.info(f"get_paths: {num}k")
     dir_paths = list(reversed(dir_paths))
-    return file_paths, dir_paths
+    return dir_paths
 
 
 def remove_all(root_dir):
     logger.info(f"get paths")
-    file_paths, dir_paths = get_paths(root_dir)
-    for file_path in tqdm(file_paths, total=len(file_paths), desc="remove file"):
-        try:
-            os.remove(file_path)
-        except:
-            continue
+    dir_paths = get_paths(root_dir)
 
     for dir_path in tqdm(dir_paths, total=len(dir_paths), desc="remove dirs"):
         shutil.rmtree(dir_path, ignore_errors=True)
 
 
 if __name__ == '__main__':
-    root_dir_path = "/root/DATA/liuchengyue/results/feature_extractor_result/decompressed_debian_mirror"
+    root_dir_path = "/Users/liuchengyue/Desktop/demo"
     remove_all(root_dir_path)
